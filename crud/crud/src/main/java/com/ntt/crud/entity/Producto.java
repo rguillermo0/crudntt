e2db3c2e9e82c81a4cacec3e06010c9e3a90cb8c
rguillermo0 <r_guillermo0@yahoo.com.mx>
2023-10-18 22:33:46 -0600

    crudntt

---
+++ crud/crud/src/main/java/com/ntt/crud/entity/Producto.java
@@ -0,0 +1,57 @@
+package com.ntt.crud.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private float precio;
+
+	public Producto() {
+	}
+
+	public Producto(String nombre, String descripcion, float precio) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+}
